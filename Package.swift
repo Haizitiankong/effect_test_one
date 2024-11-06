4a4ece648c04438acf63a88d2eab355233082c05
刘海平 <liuhaiping.haizi@bytedance.com>
2024-11-06 15:05:23 +0800

    1.1.0.8-karrot-eolite

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "EffectOneKit",
-        url: "https://sf3-ttcdn-tos.pstatp.com/obj/volcengine/EffectOneKit/1.1.0.7-karrot-eolite/EffectOneKit.xcframework.zip",
-        checksum: "d0261c13167d36a46fa1d5e48563f007229555c265abc16b9e193c5193d7296a"),
+        url: "https://sf3-ttcdn-tos.pstatp.com/obj/volcengine/EffectOneKit/1.1.0.8-karrot-eolite/EffectOneKit.xcframework.zip",
+        checksum: "16bc1288e3a87b10c561d2e7a0d63af515c8a8eacad05fc07cb890c983c0d1b9"),
   ]
 )
